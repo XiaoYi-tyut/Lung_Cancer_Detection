25570ff3a216262fa6cf21d99d2d7385e4d34741
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2017-08-30 15:28:25 -0700

    added 3d keras

--- Main_CNN.py
+++ Main_CNN.py
@@ -4,7 +4,7 @@ import csv
 import cv2
 import os
 
-from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, merge, ZeroPadding2D, AveragePooling2D, Flatten, Dropout, Dense, Activation, BatchNormalization
+from keras.layers import Input, Dense, Convolution3D, MaxPooling3D, merge, ZeroPadding3D, AveragePooling3D, Flatten, Dropout, Dense, Activation, BatchNormalization
 from keras.regularizers import l2
 from keras.models import Model, model_from_json
 from keras.optimizers import Adam
@@ -13,7 +13,7 @@ from LRN_helper import LRN2D
 
 number_of_classes = 2
 dimension = 224
-number_of_channels = 3
+number_of_channels = 1
 batch_size = 50
 features_directory = 'G:/DL/Lung-Cancer_Detection/preprocessed_images/'
 labels_directory = 'G:/DL/Lung-Cancer_Detection/???????????????/'
@@ -44,19 +44,19 @@ def load_features_and_labels_dataset(features_directory, labels_directory):
 
 def inception(input, prefix, n1x1, r3x3, n3x3, r5x5, n5x5, m1x1):
     # input = Input(shape=shape)(input)
-    layer_conv_1x1_b = Convolution2D(r3x3, 1, 1, border_mode='same', activation='relu', name=prefix+'layer_conv_1x1_b', W_regularizer=l2(0.0002))(input)
+    layer_conv_1x1_b = Convolution3D(r3x3, 1, 1, 1, border_mode='same', activation='relu', name=prefix+'layer_conv_1x1_b', W_regularizer=l2(0.0002))(input)
     layer_conv_1x1_b= BatchNormalization()(layer_conv_1x1_b)
-    layer_conv_1x1_c = Convolution2D(r5x5, 1, 1, border_mode='same', activation='relu', name=prefix+'layer_conv_1x1_c', W_regularizer=l2(0.0002))(input)
+    layer_conv_1x1_c = Convolution3D(r5x5, 1, 1, 1, border_mode='same', activation='relu', name=prefix+'layer_conv_1x1_c', W_regularizer=l2(0.0002))(input)
     layer_conv_1x1_c = BatchNormalization()(layer_conv_1x1_c)
-    layer_max_3x3_d = MaxPooling2D(pool_size=(3, 3), strides=(1, 1), border_mode='same', name=prefix+'layer_max_3x3_d')(input)
+    layer_max_3x3_d = MaxPooling3D(pool_size=(3, 3, 3), strides=(1, 1, 1), border_mode='same', name=prefix+'layer_max_3x3_d')(input)
 
-    layer_conv_1x1_a = Convolution2D(n1x1, 1, 1, border_mode='same', activation='relu', name=prefix+'layer_conv_1x1_a', W_regularizer=l2(0.0002))(input)
+    layer_conv_1x1_a = Convolution3D(n1x1, 1, 1, 1, border_mode='same', activation='relu', name=prefix+'layer_conv_1x1_a', W_regularizer=l2(0.0002))(input)
     layer_conv_1x1_a = BatchNormalization()(layer_conv_1x1_a)
-    layer_conv_3x3_b = Convolution2D(n3x3, 3, 3, border_mode='same', activation='relu', name=prefix+'layer_conv_3x3_b', W_regularizer=l2(0.0002))(layer_conv_1x1_b)
+    layer_conv_3x3_b = Convolution3D(n3x3, 3, 3, 3, border_mode='same', activation='relu', name=prefix+'layer_conv_3x3_b', W_regularizer=l2(0.0002))(layer_conv_1x1_b)
     layer_conv_3x3_b = BatchNormalization()(layer_conv_3x3_b)
-    layer_conv_5x5_c = Convolution2D(n5x5, 5, 5, border_mode='same', activation='relu', name=prefix+'layer_conv_5x5_c', W_regularizer=l2(0.0002))(layer_conv_1x1_c)
+    layer_conv_5x5_c = Convolution3D(n5x5, 5, 5, 5, border_mode='same', activation='relu', name=prefix+'layer_conv_5x5_c', W_regularizer=l2(0.0002))(layer_conv_1x1_c)
     layer_conv_5x5_c = BatchNormalization()(layer_conv_5x5_c)
-    layer_conv_1x1_d = Convolution2D(m1x1, 1, 1, border_mode='same', activation='relu', name=prefix+'layer_conv_1x1_d', W_regularizer=l2(0.0002))(layer_max_3x3_d)
+    layer_conv_1x1_d = Convolution3D(m1x1, 1, 1, 1, border_mode='same', activation='relu', name=prefix+'layer_conv_1x1_d', W_regularizer=l2(0.0002))(layer_max_3x3_d)
     layer_conv_1x1_d = BatchNormalization()(layer_conv_1x1_d)
 
     output = merge([layer_conv_1x1_a, layer_conv_3x3_b, layer_conv_5x5_c, layer_conv_1x1_d], mode='concat')
@@ -76,27 +76,27 @@ dataset_test_features = dataset_test_features.reshape((-1, dimension, dimension,
 print('dataset_test_features.shape:', dataset_test_features.shape)
 print('dataset_test_labels.shape:', dataset_test_labels.shape)
 
-# googlenet start:
+# neural network start:
 input = Input(shape=(dimension, dimension, number_of_channels))
-conv1 = Convolution2D(64, 7, 7, subsample=(2,2), border_mode='same', activation='relu', W_regularizer=l2(0.0002))(input)
-conv1 = ZeroPadding2D(padding=(1,1))(conv1)
-conv1 = MaxPooling2D(pool_size=(3,3), strides=(2,2), border_mode='valid')(conv1)
-conv1 = LRN2D()(conv1)
+conv1 = Convolution3D(64, 7, 7, 7, subsample=(2,2,2), border_mode='same', activation='relu', W_regularizer=l2(0.0002))(input)
+conv1 = ZeroPadding3D(padding=(1,1,1))(conv1)
+conv1 = MaxPooling3D(pool_size=(3,3,3), strides=(2,2,2), border_mode='valid')(conv1)
+# conv1 = LRN2D()(conv1)
 
 conv1 = BatchNormalization()(conv1)
-conv2 = Convolution2D(64, 1, 1, border_mode='same', activation='relu', W_regularizer=l2(0.0002))(conv1)
-conv2 = Convolution2D(192, 3, 3, border_mode='same', activation='relu', W_regularizer=l2(0.0002))(conv2)
-conv2 = LRN2D()(conv2)
-conv2 = ZeroPadding2D(padding=(1,1))(conv2)
-conv2 = MaxPooling2D(pool_size=(3,3), strides=(2,2), border_mode='valid')(conv2)
+conv2 = Convolution3D(64, 1, 1, 1, border_mode='same', activation='relu', W_regularizer=l2(0.0002))(conv1)
+conv2 = Convolution3D(192, 3, 3, 3, border_mode='same', activation='relu', W_regularizer=l2(0.0002))(conv2)
+# conv2 = LRN2D()(conv2)
+conv2 = ZeroPadding3D(padding=(1,1,1))(conv2)
+conv2 = MaxPooling3D(pool_size=(3,3,3), strides=(2,2,2), border_mode='valid')(conv2)
 
 conv2 = BatchNormalization()(conv2)
 inception1 = inception(conv2, '3a', 64, 96, 128, 16, 32, 32)
 
 inception1 = BatchNormalization()(inception1)
 inception2 = inception(inception1,'3b', 128, 128, 192, 32, 96, 64)
-inception2 = ZeroPadding2D(padding=(1,1))(inception2)
-inception2 = MaxPooling2D(pool_size=(3,3), strides=(2,2), border_mode='valid')(inception2)
+inception2 = ZeroPadding3D(padding=(1,1,1))(inception2)
+inception2 = MaxPooling3D(pool_size=(3,3,3), strides=(2,2,2), border_mode='valid')(inception2)
 
 inception2 = BatchNormalization()(inception2)
 inception3 = inception(inception2, '4a', 192, 96, 208, 16, 48, 64)
@@ -112,8 +112,8 @@ inception6 = inception(inception5, '4d', 112, 144, 288, 32, 64, 64)
 
 inception6 = BatchNormalization()(inception6)
 inception7 = inception(inception6, '4e', 256, 160, 320, 32, 128, 128)
-inception7 = ZeroPadding2D(padding=(1,1))(inception7)
-inception7 = MaxPooling2D(pool_size=(3,3), strides=(2,2), border_mode='valid')(inception7)
+inception7 = ZeroPadding3D(padding=(1,1,1))(inception7)
+inception7 = MaxPooling3D(pool_size=(3,3,3), strides=(2,2,2), border_mode='valid')(inception7)
 
 inception7 = BatchNormalization()(inception7)
 inception8 = inception(inception7, '5a', 256, 160, 320, 32, 128, 128)
@@ -121,7 +121,7 @@ inception8 = inception(inception7, '5a', 256, 160, 320, 32, 128, 128)
 inception8 = BatchNormalization()(inception8)
 inception9 = inception(inception8, '5b', 384, 192, 384, 48, 128, 128)
 # inception9 = ZeroPadding2D(padding=(1,1))(inception9)
-inception9 = AveragePooling2D(pool_size=(7,7), strides=(1,1), border_mode='valid')(inception9)
+inception9 = AveragePooling3D(pool_size=(7,7,7), strides=(1,1,1), border_mode='valid')(inception9)
 
 inception9 = BatchNormalization()(inception9)
 flatten = Flatten()(inception9)
