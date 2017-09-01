fixed bugs in normalization and label comparison

Main_CNN.py
@@ -75,9 +75,12 @@ for arr in dataset_labels:
     if arr[1] == 1:
         ones += 1
 
-dataset_features_max_value = max(dataset_features)
+# normalize between 0 and 1
+dataset_features_max_value = np.amax(dataset_features)
+dataset_features_min_value = np.amin(dataset_features)
 print('dataset_features_max_value:', dataset_features_max_value)
-dataset_features = dataset_features / dataset_features_max_value
+print('dataset_features_min_value:', dataset_features_min_value)
+dataset_features = (dataset_features-dataset_features_min_value) / (dataset_features_max_value - dataset_features_min_value)
 
 # reshaping due to issues not able to find
 dataset_features = dataset_features.reshape(-1, 20, dimension, dimension, number_of_channels)

Main_CNN_floydhub.py
@@ -10,7 +10,6 @@ from keras.regularizers import l2
 from keras.models import Model, model_from_json
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ModelCheckpoint
-from LRN_helper import LRN2D
 
 number_of_classes = 2
 dimension = 112
@@ -78,8 +77,8 @@ for arr in dataset_labels:
 
 
 # normalize between 0 and 1
-dataset_features_max_value = max(dataset_features)
-dataset_features_min_value = min(dataset_features)
+dataset_features_max_value = np.amax(dataset_features)
+dataset_features_min_value = np.amin(dataset_features)
 print('dataset_features_max_value:', dataset_features_max_value)
 print('dataset_features_min_value:', dataset_features_min_value)
 dataset_features = (dataset_features-dataset_features_min_value) / (dataset_features_max_value - dataset_features_min_value)